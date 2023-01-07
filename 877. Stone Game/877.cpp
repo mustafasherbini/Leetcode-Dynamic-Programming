7bed6e1f1bdf3580697c24ac37f6939a4449570e
Mustafa El-sherbiny <mostafasherbini114@gmail.com>
2023-01-07 19:37:25 +0200

    https://leetcode.com/problems/stone-game/

---
+++ 877. Stone Game/877.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+//#include <atcoder/all>
+#define f first
+#define s second
+
+using namespace std;
+using ll = long long ;
+using ld = long double ;
+const int N = 2e5+7 , M = 1e9+7;
+const double  ES = 1e-6;
+using vi = deque<int>;
+template<class T> using min_heap = priority_queue<T, std::vector<T>, std::greater<T>>;
+
+
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
+void solve() {
+
+
+
+
+}
+
+int main(){
+//    IN();
+    int t=1;
+    cin>>t;
+    while (t--){
+        solve();
+        cout<<"\n";
+    }
+
+
+    return 0;
+}
